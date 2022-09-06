96ab9a577e66c2a387f63aec1bc28cf68d5011dc
flakvard <marni@falkvard.com>
2022-09-06 22:01:01 +0200

    Moving alien fleet and going downwards

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,13 +32,13 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+            self._update_aliens()
             self._update_screen()
 
             #Get rid of bullets that have disappered.
             for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
-            print(len(self.bullets))
             
     def _check_events(self):
         """Responds to keypresses and mouse events."""
@@ -120,7 +120,26 @@ class AlienInvasion:
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
+    def _update_aliens(self):
+        """
+        Check if the fleet is at an edge,
+        then updates the position of all aliens in the fleet.
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
 
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge"""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
         
 
 if __name__ == '__main__':
